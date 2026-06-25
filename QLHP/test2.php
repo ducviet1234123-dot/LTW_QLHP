8e775f4442a734306d6a5196e416d93857b52802
Khang <ducviet1234123@gmail.com>
2026-06-25 13:57:59 +0700

    nghich ngom tren test 2

---
+++ QLHP/test2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HI KAU</title>
+</head>
+<body>
+    <p>Test create file php</p>
+    <h2>test thay doi dư lieu trong file</h2>
+</body>
+</html>
