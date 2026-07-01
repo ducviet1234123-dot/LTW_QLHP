0d29fc77d355e14e50647f9a935104cd00b8b49b
Khang <ducviet1234123@gmail.com>
2026-07-01 16:26:56 +0700

    add update

---
+++ QLHP/update.php
@@ -0,0 +1,94 @@
+<?php include("header.php"); ?>
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+require_once("mysqlConnect_3.php");
+if (!isset($_SESSION["user"])) {
+  header("Location: Dangnhap.php");
+  exit();
+}
+
+$user = $_SESSION["user"];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $year = $_POST["year"];
+    $gender = $_POST["gender"];
+    $email = $user["email"];
+
+    $thongtin = $mysqli->prepare("UPDATE users SET name=?, year=?, gender=? WHERE email=?");
+    $thongtin->bind_param("siss", $name, $year, $gender, $email);
+    $thongtin->execute();
+
+    //Cap nhat session
+    $_SESSION["user"]["name"] = $name;
+    $_SESSION["user"]["year"] = $year;
+    $_SESSION["user"]["gender"] = $gender;
+
+    header("Location: profile.php");
+    exit();
+}
+?>
+
+    <title>Cap nhat thong tin</title>
+<style>
+        fieldset{
+            margin: auto;
+            width: 600px;
+        }
+        .row{
+            display: grid;
+            grid-template-columns: 150px 1fr;
+            align-items: center;
+            margin-top: 5px;
+        }
+        label {
+            text-align: right;
+            margin-right: 10px;
+            font-weight: bolder;
+        }
+        input {
+            text-align: left;
+            padding: 4px;
+            box-sizing: border-box;
+        }
+</style>
+<form method="POST">
+  <fieldset>
+    <div style="text-align: center; color: blue">
+        <h1>Cập nhật thông tin</h1>
+    </div>
+    <div class="row">
+        <label for="email">Email:</label>
+        <?= $user["email"] ?><br>
+    </div>
+    <div class="row">
+        <label for="name">Họ tên:</label>
+        <input style="width:55%" type="text" name="name" value="<?= $user["name"] ?>"><br>
+    </div>
+    <div class="row">
+        <label for="year">Năm sinh:</label>
+        <select style="width:15%" name="year">
+        <?php
+        $nowYear = date("Y");
+        for($y = $nowYear; $y >= 1920; $y--){
+            $sel = ($y == $user["year"]) ? "selected" : "";
+            echo "<option $sel>$y</option>";
+        }
+        ?>
+    </select><br>
+    </div>
+    <div class="row">
+        <label for="gender">Giới tính:</label>
+        <div>
+            <input type="radio" name="gender" value="Nam" <?= $user["gender"]=="Nam"?"checked":"" ?>>Nam
+            <input type="radio" name="gender" value="Nữ" <?= $user["gender"]=="Nữ"?"checked":"" ?>>Nữ <br>
+        </div>
+    </div>
+    <div style="text-align: center; margin :15px">
+        <button type="submit">Cập nhật</button>
+    </div>
+  </fieldset>
+</form>
+<?php include("footer.php"); ?>   
