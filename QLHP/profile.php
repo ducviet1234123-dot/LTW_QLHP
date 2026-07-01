6d1f950e8b018b69f844fda1016b8bd510670c76
Khang <ducviet1234123@gmail.com>
2026-07-01 16:22:06 +0700

    kh

---
+++ QLHP/profile.php
@@ -0,0 +1,21 @@
+<?php include("header.php"); ?>   
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+if (!isset($_SESSION["user"])) {
+  header("Location: dangnhap.php");
+  exit();
+}else{
+$user = $_SESSION["user"];  }
+?>
+
+<title>Thong tin ca nhan</title>
+<h2>Thông tin cá nhân</h2>
+<p>Họ tên: <?= $user["name"] ?></p>
+<p>Email: <?= $user["email"] ?></p>
+<p>Năm sinh: <?= $user["year"] ?></p>
+<p>Giới tính: <?= $user["gender"] ?></p>
+<a href="update.php">Cập nhật thông tin</a> |
+<a href="Dangxuat.php">Đăng xuất</a>
+<?php include("footer.php"); ?>   
