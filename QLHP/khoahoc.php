2c4c846f4ae6f2d3146afb69cd8720129f8b4210
MinhNha <minhnha2911@gmail.com>
2026-07-17 11:15:04 +0700

    Nhả

--- QLHP/khoahoc.php
+++ QLHP/khoahoc.php
@@ -77,11 +77,11 @@ if ($is_logged_in) {
 $registered_ids = array_column($registered, 'makh');
 
 $category_titles = [
-  'toeic'    => ['Lộ trình TOEIC', 'Dành cho người cần thi chứng chỉ 2 kỹ năng nghe đọc truyền thống.', 'grad-toeic'],
-  'ielts'    => ['Lộ trình IELTS', 'Chuẩn học thuật Academic quốc tế phục vụ du học, xét tuyển.', 'grad-ielts'],
-  'vstep'    => ['Lộ trình VSTEP', 'Định dạng đề thi đánh giá năng lực ngoại ngữ theo khung chuẩn VN.', 'grad-vstep'],
-  'cambridge' => ['Lộ trình Cambridge English', 'Chuẩn quốc tế với các chứng chỉ KET, PET, FCE, CAE, CPE.', 'grad-ielts'],
-  'giaotiep' => ['Tiếng Anh Giao Tiếp', 'Phát triển phản xạ nghe nói tự nhiên cho đời sống và công việc.', 'grad-giaotiep'],
+  'toeic'    => ['Lộ trình TOEIC', 'Dành cho người cần thi chứng chỉ 2 kỹ năng nghe đọc truyền thống.',],
+  'ielts'    => ['Lộ trình IELTS', 'Chuẩn học thuật Academic quốc tế phục vụ du học, xét tuyển.',],
+  'vstep'    => ['Lộ trình VSTEP', 'Định dạng đề thi đánh giá năng lực ngoại ngữ theo khung chuẩn VN.',],
+  'cambridge' => ['Lộ trình Cambridge English', 'Chuẩn quốc tế với các chứng chỉ KET, PET, FCE, CAE, CPE.',],
+  'giaotiep' => ['Tiếng Anh Giao Tiếp', 'Phát triển phản xạ nghe nói tự nhiên cho đời sống và công việc.',],
 ];
 
 $js_courses = $all_courses;
